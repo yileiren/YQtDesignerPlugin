487efc93286955a6669816f6fb72e1745ecb67da
yileiren <yileiren@126.com>
2011-04-26 17:31:01 +0800

    编写测试程序

--- Test/mainwindow.h
+++ Test/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "../YRichEditor/yricheditor.h"
 
 namespace Ui {
     class MainWindow;
@@ -14,6 +15,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
 
 private:
     Ui::MainWindow *ui;
--- YQtDesignerPlugin.pro
+++ YQtDesignerPlugin.pro
@@ -2,5 +2,7 @@ TEMPLATE    = subdirs
 SUBDIRS     = \
             YQtDesignerPlugin_YRichEditor \
             YQtDesignerPlugin_Test
+
+
 YQtDesignerPlugin_Test.subdir = Test
 YQtDesignerPlugin_YRichEditor.subdir = YRichEditor
